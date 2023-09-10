4b13595b3d52cee981fb85bbed94b84fa687b186
LAPTOP-T4IEN2K1\TUF_Gaming <dongdinhvan.edu@gmail.com>
2023-09-10 21:46:53 +0700

    xxxxxx

---
+++ src/components/ImportUser.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ImportUser = () => {
+  return (
+    <div className='flex flex-col w-full h-full m-10 items-center justify-center bg-gray-300' >
+
+        
+
+    </div>
+  )
+}
+
+export default ImportUser
--- src/pages/configuration/ConfigurationUser.js
+++ src/pages/configuration/ConfigurationUser.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Header } from '../../components/Header'
 import NavMenu from '../../components/NavMenu'
 import { Footer } from '../../components/Footer'
+import ImportUser from '../../components/ImportUser'
 
 const ConfigurationUser = () => {
   return (
-    <div className="flex flex-col w-full h-max bg-white justify-between">
+    <div className="flex flex-col w-full min-h-screen  bg-white">
           <Header />
           <NavMenu selectedItem = "menu4"/>
+          <ImportUser/>
+
           <Footer />
         </div>
   )
